0d1d692c89baae86a73df0be921c0ca499b96f99
stwartf64 <stewartf64@gmail.com>
2020-06-15 18:03:15 -0600

    fixed select multiple with default data

--- demo/demoapp/people_group.py
+++ demo/demoapp/people_group.py
@@ -1,8 +1,9 @@
+from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView, ListView
 from django import forms
-from demoapp.models import PeopleGroup
+from demoapp.models import PeopleGroup, Person
 from djgentelella.forms.forms import CustomForm
-from djgentelella.widgets.selects import AutocompleteSelect
+from djgentelella.widgets.selects import AutocompleteSelect, AutocompleteSelectMultiple
 
 
 class PeopleGroupForm(CustomForm, forms.ModelForm):
@@ -10,22 +11,24 @@ class PeopleGroupForm(CustomForm, forms.ModelForm):
         model = PeopleGroup
         fields = '__all__'
         widgets = {
-            'people': AutocompleteSelect(baseurl="personbasename-list")
+            'people': AutocompleteSelectMultiple(baseurl="personbasename-list")
         }
 
 class PeopleGroupAdd(CreateView):
     model = PeopleGroup
     #fields = '__all__'
-    success_url = '/'
+    success_url = '/pgroup/'
     form_class = PeopleGroupForm
     template_name = 'gentelella/index.html'
+
 
 class PeopleGroupChange(UpdateView):
     model = PeopleGroup
     #fields = '__all__'
-    success_url = '/'
+    success_url = '/pgroup/'
     form_class = PeopleGroupForm
     template_name = 'gentelella/index.html'
+
 
 class PeopleGroupList(ListView):
     model = PeopleGroup
